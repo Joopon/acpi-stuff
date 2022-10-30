d2edebdc57ec088756769c546896df4a2b6aa86b
Joopon <joopon.jnw@gmail.com>
2022-10-30 17:54:25 +0100

    syntax fix in order to compile acpi_dump_info on Linux 5.19

--- acpi_dump_info/acpi_dump_info.c
+++ acpi_dump_info/acpi_dump_info.c
@@ -6,6 +6,7 @@
 #include <linux/module.h>
 #include <asm/uaccess.h>
 #include <linux/seq_file.h>
+#include <linux/proc_fs.h>
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Dumps information of the ACPI handles for devices");
@@ -46,11 +47,11 @@ static int meh_open(struct inode *inode, struct file *file) {
 	return single_open(file, meh_show, NULL);
 }
 
-static struct file_operations fops = {
-	.open    = meh_open,
-	.read    = seq_read,
-	.llseek  = seq_lseek,
-	.release = single_release,
+static struct proc_ops fops = {
+	.proc_open    = meh_open,
+	.proc_read    = seq_read,
+	.proc_lseek  = seq_lseek,
+	.proc_release = single_release,
 };
 
 static int __init meh_init(void) {
